1c0a517c6a7feed913bc30166743ce234b7d160a
ABThalawaththa <aselb046@gmail.com>
2021-04-22 04:08:09 +0530

    Test cases to test DELETE and PUT methods implemented

--- GadgetBadget/src/product/testing/ProductTests.java
+++ GadgetBadget/src/product/testing/ProductTests.java
@@ -9,13 +9,15 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class ProductTests {
-
+	
+	//Test Case to test GET Method
 	@Test
 	public void test_GET() {
 		given().get("http://localhost:8080/GadgetBadget/WebApi/Products").then().statusCode(200)
 				.body("ProductList.productId[0]", equalTo(1)).log().all();
 	}
 
+	//Test Case to test POST Method
 	@Test
 	public void test_POST() {
 		Product product = new Product();
@@ -25,12 +27,41 @@ public class ProductTests {
 		product.setProductCategory("IT");
 		product.setResercherId(2);
 
+		//request body data
 		String body = String.format(
 				"productTitle=%s&productDescription=%s&" + "productType=%s&productCategory=%s&researcherID=%s",
 				product.getProductTitle(), product.getProductDescription(), product.getProductType(), product.getProductCategory(),Integer.toString(product.getResercherId()));
 
 		given().with().header("Content-Type", "application/x-www-form-urlencoded").accept(ContentType.HTML).body(body)
 				.when().post("http://localhost:8080/GadgetBadget/WebApi/Products").then().statusCode(200);
+
+	}
+	
+	//Test case to test DELETE method
+	@Test
+	public void test_DELETE() {
+		
+		String body = String.format("productId=%s","7");
+		given().with().header("Content-Type","application/x-www-form-urlencoded").accept(ContentType.HTML).body(body)
+		.when().delete("http://localhost:8080/GadgetBadget/WebApi/Products").then().statusCode(200);
+	}
+	
+	//Test case to test PUT method
+	@Test
+	public void test_PUT() {
+		Product product = new Product();
+		product.setProductTitle("Test Product update");
+		product.setProductDescription("Test Product Description update");
+		product.setProductType("Selling");
+		product.setProductCategory("IT");
+		product.setResercherId(2);
+
+		String body = String.format(
+				"productId=%s&productTitle=%s&productDescription=%s&" + "productType=%s&productCategory=%s&researcherID=%s",
+				"8","Test Product update","Test Product Description update","Selling","IT","2");
+
+		given().with().header("Content-Type", "application/x-www-form-urlencoded").accept(ContentType.HTML).body(body)
+				.when().put("http://localhost:8080/GadgetBadget/WebApi/Products").then().statusCode(200);
 
 	}
 
