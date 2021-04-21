9d3cb13e6fd55cdc629fb09f92dac726ce3d4e8c
ABThalawaththa <aselb046@gmail.com>
2021-04-21 14:28:04 +0530

    get method tested

--- GadgetBadget/src/product/testing/ProductTests.java
+++ GadgetBadget/src/product/testing/ProductTests.java
@@ -1,9 +1,21 @@
 package product.testing;
-import org.testng.annotattions.Test;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+
 
 public class ProductTests {
 	
 	@Test
-	public void test_GET()
+	public void test_GET() {
+		given().get("http://localhost:8080/GadgetBadget/WebApi/Products").then().
+		statusCode(200).
+		body("ProductList.productId[0]",equalTo(1)).
+		log().all();
+		
+	}
 
 }
